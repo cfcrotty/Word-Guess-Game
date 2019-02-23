b72bb837fc0142e4fe5edb84c573d277d4ec034e
cfcrotty <carafeliselana@gmail.com>
2019-02-22 21:07:18 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ var lossesScore = 0;
 var guessLeftScore = 10;
 var nextSong = 0;
 var guessLetter = [];
-var newSong = 1;
 var lettersArr = [];
 var audioLoaded = 0;
 
@@ -15,6 +14,7 @@ var numOfGuessesText = document.getElementById("numOfGuesses");
 var lettersText = document.getElementById("letters");
 var changeAudio = document.getElementById("audioSrc").src;
 
+//object songs has the song properties
 var songs = [{s:"Bohemian Rhapsody",a:"assets/audio/Bohemian Rhapsody.mp3",h:"assets/images/br.jpg"},
             {s:"Blurred Lines",a:"assets/audio/Blurred Lines.mp3",h:"assets/images/bl.jpeg"},
             {s:"Sugar Man",a:"assets/audio/Sugar Man.mp3",h:"assets/images/sm.jpg"},
@@ -26,70 +26,83 @@ var songs = [{s:"Bohemian Rhapsody",a:"assets/audio/Bohemian Rhapsody.mp3",h:"as
 // Creates an array that lists alphabet.
 var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//returns and generates HTML code(span) depending argument length
-function printSpan(song) {
-    var txt = "";
-    for (var i=0;i<song.length;i++) {
-        txt += "<span id='span"+i+"'>";
-        txt += (song[i]===" ") ? "&nbsp;&nbsp;&nbsp;" : " _ "; //song[i].match(/^ *$/)
-        txt += "</span>";
-    }
-    currentWordText.innerHTML=txt;
-    //return txt;
-}
+//object wordGame has functions used in the game
+var wordGame = {
+    //flag for next song
+    newSong: 1,
 
-//prints the letter in th HTML span
-function printLetter(usrGuess,songIdx) {
-    for (var i=0;i<songs[songIdx].s.length;i++) {
-        if (usrGuess===songs[songIdx].s[i].toLowerCase()) {
-            document.getElementById("span"+i).textContent=usrGuess;
-            guessLetter[i]=usrGuess;
+    //returns and generates HTML code(span) depending argument length
+    printSpan: function(song) {
+        var txt = "";
+        for (var i=0;i<song.length;i++) {
+            txt += "<span id='span"+i+"'>";
+            txt += (song[i]===" ") ? "&nbsp;&nbsp;&nbsp;" : " _ "; //song[i].match(/^ *$/)
+            txt += "</span>";
+        }
+        currentWordText.innerHTML=txt;
+        //return txt;
+    },
+
+    //prints the letter in th HTML span
+    printLetter: function(usrGuess,songIdx) {
+        for (var i=0;i<songs[songIdx].s.length;i++) {
+            if (usrGuess===songs[songIdx].s[i].toLowerCase()) {
+                document.getElementById("span"+i).textContent=usrGuess;
+                guessLetter[i]=usrGuess;
+            }
+        }
+        
+    },
+
+    //resets counters, HTML span and change song
+    resetAll: function() {
+        currentWordText.innerHTML="";
+        guessLetter = [];
+        lettersArr = [];
+        wordGame.newSong = 1;
+        guessLeftScore = 10;
+        numOfGuessesText.textContent = 10;
+        lettersText.textContent = "";
+        wordGame.printSpan(songs[nextSong].s.toLowerCase());
+    },
+
+    //function to load/change song
+    loadSong: function() {
+        if (!(songs.length <= nextSong)) {
+            changeAudio=songs[nextSong].a; 
+            myAudio.src=songs[nextSong].a; 
+            myAudio.play(); 
+        }
+    },
+
+    //show or hide image
+    displayHideHint: function(val) {
+        if (songs.length <= nextSong) return;
+        if (val) {
+            var displayImage = document.getElementById("imageHint");
+            var image = document.getElementById("image");
+            image.src = songs[nextSong].h;
+            displayImage.style.display="block";
+            document.getElementById("hint").style.display = "block";
+        } else if (!val) {
+            document.getElementById("imageHint").style.display = "none";
+            document.getElementById("hint").style.display = "none";
         }
     }
-    
-}
+};
 
-//resets counters, HTML span and change song
-function resetAll() {
-    currentWordText.innerHTML="";
-    guessLetter = [];
-    lettersArr = [];
-    newSong = 1;
-    guessLeftScore = 10;
-    numOfGuessesText.textContent = 10;
-    lettersText.textContent = "";
-    printSpan(songs[nextSong].s.toLowerCase());
-}
-
-//function to load/change song
-function loadSong() {
-    changeAudio=songs[nextSong].a; 
-    myAudio.src=songs[nextSong].a; 
-    myAudio.play(); 
-}
-
-//show or hide image
-function displayHideHint(val) {
-    if (val) {
-        var displayImage = document.getElementById("imageHint");
-        var image = document.getElementById("image");
-        image.src = songs[nextSong].h;
-        displayImage.style.display="block";
-        document.getElementById("hint").style.display = "block";
-    } else if (!val) {
-        document.getElementById("imageHint").style.display = "none";
-        document.getElementById("hint").style.display = "none";
-    }
-}
-
-resetAll();
+wordGame.resetAll();
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
     if (songs.length>nextSong) {
-        if (newSong) {  
-            if (!audioLoaded) {loadSong(); audioLoaded++;} else {audioLoaded=0;}
-            resetAll(); 
-            newSong=0;
+        if (wordGame.newSong) {  
+            if (!audioLoaded) {
+                wordGame.loadSong(); audioLoaded++;
+            } else {
+                audioLoaded=0;
+            }
+            wordGame.resetAll(); 
+            wordGame.newSong=0;
         }
 
         var userGuess = event.key;
@@ -111,29 +124,30 @@ document.onkeyup = function (event) {
                         lossesScore++;
                         lossesTally.textContent = lossesScore;
                         nextSong++;
-                        loadSong();
+                        wordGame.loadSong();
                         audioLoaded++;
-                        resetAll();
+                        if (!(songs.length <= nextSong)) wordGame.resetAll();
                     }
                 } else if (chk>=0) {
-                    printLetter(userGuess,nextSong);
+                    wordGame.printLetter(userGuess,nextSong);
                     lettersArr.push(userGuess);
                 }
             }
         } 
         
         var userWord = guessLetter.toString().toLowerCase().replace(/[,\s]/g, '');
-        if (songs[nextSong].s.toLowerCase().replace(/[ \s]/g, '')===userWord) {
+        if (!(songs.length <= nextSong) && songs[nextSong].s.toLowerCase().replace(/[ \s]/g, '')===userWord) {
             winScore++;
             winsTally.textContent = winScore;
             nextSong++;
-            loadSong();
+            wordGame.loadSong();
             audioLoaded++;
-            if (songs.length>nextSong) resetAll();
+            if (!(songs.length <= nextSong)) wordGame.resetAll();
         }
     } else {
         alert("Game Over");
         myAudio.pause(); 
-        return;
+        //return;
     }
+    //alert(nextSong);
 };
